ebb6a91266fa851a55401f52c084dc8b3443f85a
Zalelin3 <shihaol@students.pitzer.edu>
2021-12-03 09:38:56 +0800

    remove for new deploy setting

--- YYJS/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    configureWebpack: {
-        devtool: 'source-map'
-    },
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/yyjs/'  //production path
-        : '/'  //development path
-}
